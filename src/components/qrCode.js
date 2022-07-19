1b806fbbb5f31b28fff9ed0338ce87dcf69ac254
Cody De Arkland <codydearkland@gmail.com>
2022-07-18 23:25:18 -0700

    Resolving conflicts

--- src/components/qrCode.js
+++ src/components/qrCode.js
@@ -1,14 +1,13 @@
 import QRCode from "react-qr-code";
 
-// const QRURL = "https://codyde.github.io/ld-demo-app/";
+const QRURL = "https://codyde.github.io/ld-demo-app/";
 // const QRURL = "https://codydemoapp.netlify.app"
 
-const qrCodeHome = (flags) => {  
-  console.log(flags.codeurl)
+const qrCodeHome = () => {  
   return (
     <div className="mx-auto shadow-2xl">
       <div className="qr-wrapper">
-        <QRCode size="200" value={flags.codeurl} />
+        <QRCode size="200" value={QRURL} />
       </div>
     </div>
   )     
